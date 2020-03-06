b152e413217b082b9e23d668f44823bb5ec6ee7b
BenStoever <stoever@bioinfweb.info>
2020-03-06 18:06:29 +0000

    Some stubs added.

---
+++ main/src/info/bioinfweb/osrfilter/Main.java
@@ -0,0 +1,16 @@
+package info.bioinfweb.osrfilter;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		// Load trees.
+		// Perform pairwise comparisons, including calculating a shared taxon set and rerooting one of each pair to match the other. (Note the different root topologies which are identical if not root is assumed.)
+		// - Later, each comparison should be in a separate thread and all cores should be used.
+		// - Tree iteration:
+		//   - Use two readers. One for the first and one for the second tree to be compared. The first one reads all trees once. For each tree from 
+		//     the first reader, a new second reader is created that reads all trees behind the first one.
+		//   - The iteration may also run over separate files.
+		// Output results.
+	}
+}
---
+++ main/src/info/bioinfweb/osrfilter/io/TreeIterator.java
@@ -0,0 +1,30 @@
+package info.bioinfweb.osrfilter.io;
+
+
+import java.util.Iterator;
+
+import info.bioinfweb.jphyloio.JPhyloIOEventReader;
+import info.bioinfweb.treegraph.document.Tree;
+
+
+
+/**
+ * Iterates over all trees in a set of files.
+ * 
+ * @author Ben St&ouml;ver
+ */
+public class TreeIterator implements Iterator<Tree>{
+	private JPhyloIOEventReader reader;
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Tree next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}

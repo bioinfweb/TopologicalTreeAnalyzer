b339611e5f16ed34476141e1751798a1f23e4937
BenStoever <stoever@bioinfweb.info>
2020-03-12 14:42:46 +0000

    PairAnalyzer now considers shared terminal set.

--- main/src/info/bioinfweb/osrfilter/analysis/PairAnalyzer.java
+++ main/src/info/bioinfweb/osrfilter/analysis/PairAnalyzer.java
@@ -1,6 +1,9 @@
 package info.bioinfweb.osrfilter.analysis;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 import info.bioinfweb.osrfilter.data.PairComparison;
 import info.bioinfweb.osrfilter.data.SplitsTree;
 import info.bioinfweb.treegraph.document.topologicalcalculation.LeafSet;
@@ -9,12 +12,21 @@ import info.bioinfweb.treegraph.document.topologicalcalculation.LeafSet;
 
 public class PairAnalyzer {
 	public PairComparison analyzePair(SplitsTree tree1, SplitsTree tree2) {
-		//TODO Should different terminal sets be supported? -> Yes. This would not allow for a reusable HashMap, except if many trees still share the same taxon set.
+		LeafSet sharedTerminals = tree1.getTerminalSet().and(tree2.getTerminalSet());
 		
+		// Create set for shared terminal leaf sets of tree 2 to be reused for each leaf set in tree1:
+		Set<LeafSet> comparedSplits = new HashSet<LeafSet>(tree1.getSplits().size());
+		for (LeafSet leafSet2 : tree2.getSplits()) {
+			comparedSplits.add(leafSet2.and(sharedTerminals));
+		}  //TODO This map might be stored and later reused if many trees shared the same leaf set.
+
+		// Compare splits:
 		int matchingSplits = 0;
 		int conflictingSplits = 0;
-		for (LeafSet leafSet : tree1.getSplits()) {
-			if (tree2.getSplits().contains(leafSet) || tree2.getSplits().contains(leafSet.complement())) {
+		for (LeafSet leafSet1 : tree1.getSplits()) {
+			leafSet1 = leafSet1.and(sharedTerminals);
+
+			if (comparedSplits.contains(leafSet1) || comparedSplits.contains(leafSet1.complement())) {
 				matchingSplits++;
 			}
 			else {
@@ -22,6 +34,6 @@ public class PairAnalyzer {
 			}
 		}
 		
-		return new PairComparison(matchingSplits, conflictingSplits, -1); //TODO Determine number of terminals.
+		return new PairComparison(matchingSplits, conflictingSplits, sharedTerminals.childCount());
 	}
 }

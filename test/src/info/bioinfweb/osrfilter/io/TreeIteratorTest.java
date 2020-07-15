e8615131b728ce251a8bce6f1e5edbaf0f9365ec
BenStoever <stoever@bioinfweb.info>
2020-07-15 13:07:14 +0000

    Test project with TreeIteratorTest added.

---
+++ test/src/info/bioinfweb/osrfilter/io/TreeIteratorTest.java
@@ -0,0 +1,24 @@
+package info.bioinfweb.osrfilter.io;
+
+
+import org.junit.* ;
+
+import static org.junit.Assert.* ;
+
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class TreeIteratorTest {
+	@Test
+	public void testReading() throws IOException, Exception {
+		TreeIterator iterator = new TreeIterator(new File("data/Tree1.tre"), new File("data/Tree2.tre"));
+		
+		for (int i = 1; i <= 6; i++) {
+			assertTrue(iterator.hasNext());
+			System.out.println(iterator.next().getTreeIdentifier());
+		}
+		assertFalse(iterator.hasNext());
+	}
+}

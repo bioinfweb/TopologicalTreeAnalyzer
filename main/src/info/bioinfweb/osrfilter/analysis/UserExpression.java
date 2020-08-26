0b45a0dca022d23cb0a0643cab6c5e8a1ac83a3b
BenStoever <stoever@bioinfweb.info>
2020-08-26 10:10:38 +0000

    Reverted previous revision.

--- main/src/info/bioinfweb/osrfilter/analysis/UserExpression.java
+++ main/src/info/bioinfweb/osrfilter/analysis/UserExpression.java
@@ -7,18 +7,25 @@ import org.nfunk.jep.Node;
 
 public class UserExpression {
 	private boolean treeTarget;
+	private String expression;
 	private Node root;
 	
 	
-	public UserExpression(boolean hasTreeTarget, Node root) {
+	public UserExpression(boolean hasTreeTarget, String expression, Node root) {
 		super();
 		this.treeTarget = hasTreeTarget;
+		this.expression = expression;
 		this.root = root;
 	}
 
 
 	public boolean hasTreeTarget() {
 		return treeTarget;
+	}
+
+
+	public String getExpression() {
+		return expression;
 	}
 
 

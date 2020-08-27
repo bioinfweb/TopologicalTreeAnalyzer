c1988a9238b16bec76bfaf7ab5b92da55a86a831
BenStoever <stoever@bioinfweb.info>
2020-08-27 12:27:30 +0000

    Initial implementation of Main added. (Tree file output remains to be done.)

--- main/src/info/bioinfweb/osrfilter/Main.java
+++ main/src/info/bioinfweb/osrfilter/Main.java
@@ -2,18 +2,46 @@ package info.bioinfweb.osrfilter;
 
 
 import java.io.File;
-import java.util.Map;
 
 import info.bioinfweb.osrfilter.analysis.TopologicalAnalyzer;
-import info.bioinfweb.osrfilter.data.PairComparisonData;
-import info.bioinfweb.osrfilter.data.TreePair;
+import info.bioinfweb.osrfilter.analysis.UserExpressionsManager;
+import info.bioinfweb.osrfilter.data.AnalysesData;
+import info.bioinfweb.osrfilter.data.parameters.AnalysisParameters;
+import info.bioinfweb.osrfilter.io.TableWriter;
 import info.bioinfweb.osrfilter.io.TreeIterator;
-import info.bioinfweb.treegraph.document.undo.CompareTextElementDataParameters;
+import info.bioinfweb.osrfilter.io.parameters.AnalysisParameterIO;
 
 
 
 public class Main {
 	public static void main(String[] args) {
-		
+		if (args.length >= 1) {
+			try {
+				AnalysisParameters parameters = AnalysisParameterIO.getInstance().read(new File(args[0]));
+				AnalysesData analysesData = new AnalysesData();
+				
+				new TopologicalAnalyzer(parameters.getTextComparisonParameters()).compareAll(parameters.getGroupSize(), 
+						new TreeIterator(parameters.getTreeFilesNames().toArray(new String[parameters.getTreeFilesNames().size()])), analysesData);
+				
+				UserExpressionsManager manager = new UserExpressionsManager();
+				manager.setExpressions(parameters.getUserExpressions());
+				manager.evaluateExpressions(analysesData);
+				
+				parameters.getOutputDirectory().mkdirs();
+				TableWriter tableWriter = new TableWriter();
+				tableWriter.writeTreeData(new File(parameters.getOutputDirectory().getAbsolutePath() + File.separator + "TreeData.txt"), 
+						parameters.getTreeExportColumns(), analysesData.getTreeMap());
+				tableWriter.writePairData(new File(parameters.getOutputDirectory().getAbsolutePath() + File.separator + "PairData.txt"), 
+						parameters.getPairExportColumns(), analysesData.getComparisonMap());
+				//throw new IllegalArgumentException("The specified output location \"" + outputDirectory.getAbsolutePath() + "\" is not a directory.");
+			} 
+			catch (Exception e) {
+				System.out.println("The error \"" + e.getLocalizedMessage() + "\" occurred.");
+				e.printStackTrace(System.err);
+			}
+		}
+		else {
+			System.out.println("A parameter file needs to be specified.");
+		}
 	}
 }

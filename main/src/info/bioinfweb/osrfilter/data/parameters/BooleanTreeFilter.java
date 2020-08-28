0afb4e0c3ac25add8c4a7cdc553970395e515d81
BenStoever <stoever@bioinfweb.info>
2020-08-28 12:07:17 +0000

    Removed unnecessary annotation.

--- main/src/info/bioinfweb/osrfilter/data/parameters/BooleanTreeFilter.java
+++ main/src/info/bioinfweb/osrfilter/data/parameters/BooleanTreeFilter.java
@@ -1,12 +1,7 @@
 package info.bioinfweb.osrfilter.data.parameters;
 
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 
-
-
-@XmlAccessorType(XmlAccessType.FIELD)
 public class BooleanTreeFilter extends TreeFilter {
 	public BooleanTreeFilter() {
 		super();

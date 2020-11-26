c6c0516a7495c7de56558e5b38da52e5380b56c4
BenStoever <stoever@bioinfweb.info>
2020-11-26 15:15:05 +0000

    MANIFEST.MF updated. Version information updated.

--- main/src/info/bioinfweb/tta/Main.java
+++ main/src/info/bioinfweb/tta/Main.java
@@ -42,7 +42,7 @@ public class Main extends ProgramMainClass {
 	
 	
 	private Main() {
-		super(new ApplicationVersion(0, 3, 0, 258, ApplicationType.ALPHA));
+		super(new ApplicationVersion(0, 3, 0, 259, ApplicationType.ALPHA));
 	}
 	
 	

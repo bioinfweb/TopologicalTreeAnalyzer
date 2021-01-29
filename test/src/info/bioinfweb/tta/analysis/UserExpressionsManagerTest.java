7384cfc12299bbe7e329b0bc41fdced6e7dcf9fc
BenStoever <stoever@bioinfweb.info>
2021-01-29 17:08:18 +0000

    Minor changes.

--- test/src/info/bioinfweb/tta/analysis/UserExpressionsManagerTest.java
+++ test/src/info/bioinfweb/tta/analysis/UserExpressionsManagerTest.java
@@ -19,11 +19,7 @@
 package info.bioinfweb.tta.analysis;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
